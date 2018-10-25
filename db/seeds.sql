11303d633f05b408a4a258c6bda085d2e01b7d80
Peter David Zafonte <pzafonte@users.noreply.github.com>
2018-10-24 23:51:21 -0400

    initial upload

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Classic Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Californian Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Brunch Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
